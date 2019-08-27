144f5f26baaed88c96c376f73d2675f6ed2441ee
PotapenkoOleg <PotapenkoOleg@gmail.com>
2019-08-26 22:39:29 -0400

    Initial commit

---
+++ libalgosw/libalgosw/libalgosw.h
@@ -0,0 +1,19 @@
+//
+//  libalgosw.h
+//  libalgosw
+//
+//  Created by Oleg Potapenko on 8/26/19.
+//  Copyright © 2019 Oleg Potapenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libalgosw.
+FOUNDATION_EXPORT double libalgoswVersionNumber;
+
+//! Project version string for libalgosw.
+FOUNDATION_EXPORT const unsigned char libalgoswVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libalgosw/PublicHeader.h>
+
+
